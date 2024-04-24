22a796ebb5d4345188befc6363823265ea211529
jaydenthelwell <jaydenthelwell02@gmail.com>
2024-04-24 20:32:30 +0100

    Update app.py to use Heroku's PORT

--- app/app.py
+++ app/app.py
@@ -467,4 +467,8 @@ def score():
     return render_template('score.html', message=message, feedback=feedback, total_score=total_score)
 
 if __name__ == "__main__":
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
     app.run(debug=True)
