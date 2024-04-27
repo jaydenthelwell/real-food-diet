2a1f17e7a5ac06bbc257501d71a1356e6e93ffe5
jaydenthelwell <jaydenthelwell02@gmail.com>
2024-04-27 15:27:41 +0100

    Web design

--- app/app.py
+++ app/app.py
@@ -577,4 +577,4 @@ def score():
     return render_template('score.html', message=message, feedback=feedback, total_score=total_score)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5001)))
